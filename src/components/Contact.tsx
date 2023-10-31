d229de8c61bb642f6ef2eaa14b7bf60b6ce46af0
tuliodev <resendetulio51@gmail.com>
2023-10-31 12:36:36 -0300

    feature: create Contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,50 @@
+import {
+  GithubOriginalIcon,
+  GooglePlainIcon,
+  LinkedinPlainIcon,
+} from "react-devicons";
+
+export default function Contact() {
+  const data = [
+    {
+      url: "https://www.linkedin.com/in/tulio-resende-08ba441a7",
+      component: <LinkedinPlainIcon color="white" size="2em" />,
+      content: "Tulio Resende",
+    },
+    {
+      url: "https://github.com/tuliodev",
+      component: <GithubOriginalIcon color="white" size="2em" />,
+      content: "tuliodev",
+    },
+    {
+      url: "mailto:resendetuliodev@gmail.com",
+      component: <GooglePlainIcon color="white" size="2em" />,
+      content: "resendetuliodev@gmail.com",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-3 max-w-full sm:w-5/6">
+      {data.map((data) => {
+        return (
+          <div className=" border border-opacity-25 border-gray-600 p-4 opcaity-1">
+            <div className="flex flex-wrap items-center gap-3 hover:opacity-25 cursor-pointer ">
+              <a href={data.url} target="_blank" rel="noopener noreferrer">
+                {" "}
+                {data.component}
+              </a>
+              <a
+                href={data.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 text-xs"
+              >
+                {data.content}
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
