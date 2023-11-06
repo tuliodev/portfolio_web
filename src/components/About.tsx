3c661c5cb4d516b4e5323a6228e3a3c117c4d492
tuliodev <resendetulio51@gmail.com>
2023-11-06 11:00:30 -0300

    refactor: change about components style

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <p className="text-white ">
+    <p className="text-white w-3/4">
       Olá! Sou Tulio, um apaixonado Desenvolvedor Fullstack baseado no Brasil
       com mais de 4 anos de experiência na indústria de tecnologia. Minha
       especialização está na criação de aplicações web robustas e inovadoras
