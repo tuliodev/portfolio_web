e6686cc53438972a0300d583784f1aeb7de074f6
tuliodev <resendetulio51@gmail.com>
2023-11-16 15:11:39 -0300

    fix: fix dark mode issues

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,9 +1,9 @@
-import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useCallback, useMemo } from "react";
 import { useTheme } from "next-themes";
 
-import Particles from "react-tsparticles"; // Verifique se está utilizando a biblioteca correta
+import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import { Engine, ISourceOptions } from "tsparticles-engine";
+import { Engine } from "tsparticles-engine";
 
 import { darkParticlesConfig, lightParticlesConfig } from "@/config/particles";
 
@@ -14,24 +14,16 @@ export default function ParticlesBackground() {
 
   const { theme } = useTheme();
 
-  const configTheme = useRef<ISourceOptions>(darkParticlesConfig);
-
-  useEffect(() => {
-    if (theme == "dark") {
-      configTheme.current = lightParticlesConfig;
-    } else {
-      configTheme.current = darkParticlesConfig;
-    }
+  const configTheme = useMemo(() => {
+    return theme === "dark" ? darkParticlesConfig : lightParticlesConfig;
   }, [theme]);
-
-  const config = useMemo(() => configTheme.current, [configTheme.current]);
 
   return (
     <div id="particle-background">
       <Particles
         id="tsparticles"
         init={particlesInit}
-        options={config}
+        options={configTheme}
         height="auto"
         width="auto"
       />
